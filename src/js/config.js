6b98980a70e16ee0c62114b0a3a8efb759047127
AshB <ashbasn@gmaill.com>
2023-10-29 23:49:21 -0500

    Added config.js and helpers.js files

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+// Put important variables here that are responsible for defining the application
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
---
+++ src/js/helpers.js
@@ -0,0 +1,13 @@
+export const getJSON = async function(url) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`)
+    return data;
+  } catch(err) {
+    // console.log(err);
+    // to ensure the errors in helper.js , for example if the recipe id is incorrect:
+    // throw a new error so that it can be observed in mode.js catch
+    throw(err);
+  }
+}
